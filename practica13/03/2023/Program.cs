e0183a90ac3ddd692cac856dcc4fe2db1015a05e
ALbaz145 <124205704+ALbaz145@users.noreply.github.com>
2023-03-13 22:05:22 +0000

    Se creo programa de Dominos practica del dia 13 de marzo de 2023

---
+++ practica13/03/2023/Program.cs
@@ -0,0 +1,29 @@
+﻿class Dominoes
+{
+        int x;
+        int y;
+    public Dominoes(int uno, int dos)
+    {
+        x = uno;
+        y = dos;
+    }
+
+
+public static int operator + (Dominoes dom1, Dominoes dom2)
+{
+    return dom1.x + dom1.y + dom2.x + dom2.y;
+}
+
+
+
+}
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Dominoes domino1 = new Dominoes(1,3);
+        Dominoes domino2 = new Dominoes(4,3);
+        Dominoes.Equals(domino1, domino2);
+        Console.WriteLine(domino1 + domino2);
+    }
+}
